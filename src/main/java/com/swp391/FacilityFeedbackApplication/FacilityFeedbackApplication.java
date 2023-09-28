4b713887d6a782c9ea2104ff498940295fcbb787
khanghtse <khanghoang090703@gmail.com>
2023-09-28 22:03:18 +0700

    application

---
+++ src/main/java/com/swp391/FacilityFeedbackApplication/FacilityFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.swp391.FacilityFeedbackApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilityFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilityFeedbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swp391/FacilityFeedbackApplication/FacilityFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp391.FacilityFeedbackApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilityFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
